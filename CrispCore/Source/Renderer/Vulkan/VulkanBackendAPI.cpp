4509229631312b78df42d2ca5484863d98e023da
Christos Mark <baratheoniron@gmail.com>
2025-06-28 12:32:09 -0400

    Vulkan backend creates a physical/logical device, surface and swapchain

--- CrispCore/Source/Renderer/Vulkan/VulkanBackendAPI.cpp
+++ CrispCore/Source/Renderer/Vulkan/VulkanBackendAPI.cpp
@@ -5,8 +5,22 @@
 
 namespace Crisp
 {
+	// Vulkan functions only in this module
+	
+	/*
+		Vulkan Initialization Steps
+
+		1. Create Instance and DebugMsger
+		2. Query for Physical Device
+		3. Create Logical Device from Physical Device
+		4. Create Swapchain and Images
+	
+	*/
+
 	bool VulkanBackend::InitializeRendererBackendAPI(const RendererSettings& renderer_init_settings)
 	{
+
+		// Step 1 ___
 		vkb::InstanceBuilder inst_builder;
 
 		auto built_inst = inst_builder.set_app_name("Test")
@@ -74,7 +88,90 @@ namespace Crisp
 			return false;
 		}
 
+		CRISP_LOG_INFO("Created VkSurface");
 
+		// Step 2 ___
+		VkPhysicalDeviceVulkan13Features features13 = {};
+		features13.sType = VK_STRUCTURE_TYPE_PHYSICAL_DEVICE_VULKAN_1_3_FEATURES;
+		features13.dynamicRendering = true;
+		features13.synchronization2 = true;
+
+		VkPhysicalDeviceVulkan12Features features12 = {};
+		features12.sType = VK_STRUCTURE_TYPE_PHYSICAL_DEVICE_VULKAN_1_2_FEATURES;
+		features12.bufferDeviceAddress = true;
+		features12.descriptorIndexing = true;
+
+		vkb::PhysicalDeviceSelector selector(vkb_inst);
+		auto built_physical_device = selector
+			.set_minimum_version(1, 4)
+			.set_required_features_13(features13)
+			.set_required_features_12(features12)
+			.set_surface(m_Surface)
+			.select();
+
+		if(!built_physical_device)
+		{
+			CRISP_LOG_CRITICAL("Failed to select suitable physical device");
+			return false;
+		}
+
+		vkb::PhysicalDevice physical_device = built_physical_device.value();
+		const char* selected_gpu_name = physical_device.name.c_str();
+
+		CRISP_LOG_INFO("Selected device: {0}", selected_gpu_name);
+
+		// Step 3 ___
+		vkb::DeviceBuilder logical_device_builder(physical_device);
+		auto built_logical_device = logical_device_builder.build();
+
+		if (!built_logical_device)
+		{
+			CRISP_LOG_CRITICAL("Failed to build logical device from selected: {0}", selected_gpu_name);
+			return false;
+		}
+
+		vkb::Device logical_device = built_logical_device.value();
+
+		CRISP_LOG_INFO("Created logical device");
+
+		m_Device.logical = logical_device.device;
+		m_Device.physical = physical_device.physical_device;
+
+		// Step 4 ___
+		vkb::SwapchainBuilder swapchain_builder(m_Device.physical, m_Device.logical, m_Surface);
+		m_Swapchain.image_format = VK_FORMAT_B8G8R8A8_UNORM;
+
+		VkSurfaceFormatKHR desired_format = {static_cast<VkFormat>(m_Swapchain.image_format), VK_COLOR_SPACE_SRGB_NONLINEAR_KHR};
+		auto built_swapchain = swapchain_builder
+			.set_desired_format(desired_format)
+			.set_desired_present_mode(VK_PRESENT_MODE_FIFO_KHR) // TODO Hardcoded VSync present
+			.set_desired_extent(renderer_init_settings.w, renderer_init_settings.h)
+			.add_image_usage_flags(VK_IMAGE_USAGE_TRANSFER_DST_BIT)
+			.build();
+
+		if (!built_swapchain)
+		{
+			CRISP_LOG_CRITICAL("Failed to create swapchain with desired settings");
+			return false;
+		}
+		vkb::Swapchain swapchain = built_swapchain.value();
+		//		grab handle, extent dimensions and images
+		m_Swapchain.ext_w = swapchain.extent.width;
+		m_Swapchain.ext_h = swapchain.extent.height;
+
+		m_Swapchain.handle = swapchain.swapchain;
+
+		std::vector<VkImage> images = swapchain.get_images().value();
+		std::vector<VkImageView> image_views = swapchain.get_image_views().value();
+
+		for (int i = 0; i < images.size(); i++)
+		{
+			m_Swapchain.images[i] = images[i];
+			m_Swapchain.image_views[i] = image_views[i];
+			m_Swapchain.image_count++;
+		}
+
+		CRISP_LOG_INFO("Created Swapchain with desired formats, {0} images and {1}x{2}", m_Swapchain.image_count, m_Swapchain.ext_w, m_Swapchain.ext_h);
 
 		return true;
 	}
@@ -82,7 +179,16 @@ namespace Crisp
 	void VulkanBackend::ShutdownRendererBackendAPI()
 	{
 		CRISP_LOG_INFO("Shutting down Vulkan Backend");
+		vkDestroySwapchainKHR(m_Device.logical, m_Swapchain.handle, nullptr);
+		for (int i = 0; i < m_Swapchain.image_count; i++)
+		{
+			vkDestroyImageView(m_Device.logical, m_Swapchain.image_views[i], nullptr);
+		}
+		m_Swapchain.image_count = 0;
+
 		vkDestroySurfaceKHR(m_Instance, m_Surface, nullptr);
+		vkDestroyDevice(m_Device.logical, nullptr);
+
 		vkb::destroy_debug_utils_messenger(m_Instance, m_DebugMessenger);
 		vkDestroyInstance(m_Instance, nullptr);
 	}
--- CrispCore/Source/Renderer/Vulkan/VulkanBackendAPI.h
+++ CrispCore/Source/Renderer/Vulkan/VulkanBackendAPI.h
@@ -7,7 +7,15 @@ namespace Crisp
 	//		Vulkan specific types
 	typedef struct VkInstance_T* VkInstance;
 	typedef struct VkDebugUtilsMessengerEXT_T* VkDebugUtilsMessengerEXT;
+	//__
 	typedef struct VkSurfaceKHR_T* VkSurfaceKHR;
+	typedef struct VkDevice_T* VkDevice;
+	typedef struct VkPhysicalDevice_T* VkPhysicalDevice;
+	//__
+	typedef struct VkSwapchainKHR_T* VkSwapchainKHR;
+	typedef struct VkImage_T* VkImage;
+	typedef struct VkImageView_T* VkImageView;
+	
 
 	//		Crisp Engine types
 	struct RendererSettings;
@@ -26,7 +34,25 @@ namespace Crisp
 	protected:
 		VkInstance m_Instance = nullptr;
 		VkDebugUtilsMessengerEXT m_DebugMessenger = nullptr;
+		//__
 		VkSurfaceKHR m_Surface = nullptr;
-
+		struct RendererDevice
+		{
+			VkDevice logical = nullptr;
+			VkPhysicalDevice physical = nullptr;
+		} m_Device;
+		//__
+		// TODO Rearrange this
+		struct Swapchain
+		{
+			VkSwapchainKHR handle;
+			VkImage images[3];
+			VkImageView image_views[3];
+			uint32_t ext_w;
+			uint32_t ext_h;
+			uint32_t image_format;
+			uint32_t image_count = 0;
+			uint8_t max_frames_in_flight;
+		} m_Swapchain;
 	};
 }
